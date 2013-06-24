4e2e8900ab72c45d9289280fe465904d8491d3d2
b00gizm <b00gizm@gmail.com>
2013-06-24 14:09:07 +0200

    Initial commit

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,12 @@
-# This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+# Public: Install Kaleidoscope.app to /Applications.
+#
+# Examples
+#
+#   include cornerstone
+
+class kaleidoscope {
+  package { 'Kaleidoscope':
+    provider => 'compressed_app',
+    source   => 'http://cdn.kaleidoscopeapp.com/releases/Kaleidoscope-2.0.1-114.zip',
+  }
 }
